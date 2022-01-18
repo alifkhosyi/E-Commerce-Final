02175d821b14f24c3c4e48102f8377f1c509b658
com.AlifKhosyi <alifkhosyi@gmail.com>
2022-01-18 16:50:57 +0800

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Alif Khosyi Rahmatullah on 18/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
